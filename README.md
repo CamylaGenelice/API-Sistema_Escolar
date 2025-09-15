cbe460b315208603bf0a949c2f2cc73588e5b5e2
Camyla Genelice <genelicecamyla@gmail.com>
2025-09-15 18:00:35 -0300

    1.1.0
    mudanças nos nomes das pastas principais e adição das páginas de cadastro de turma e disciplina

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 As melhorias estão sendo implementadas aos poucos 👍\
 O back-end já esta pronto e funcionando, o front end está sendo feito com carinho 🙂\
-A pasta src é onde está o back e a pasta view é onde está o front. Estou utilizando o Vue.js junto com o Vite, pois achei simples e fácil de aplicar dentro do projeto. 
+Estou utilizando o Vue.js junto com o Vite, pois achei simples e fácil de aplicar dentro do projeto. 
---
+++ front-end/src/views/CadastroTurma.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex justify-content-center align-items-center cor-body" style="min-height: 100vh;">
+        <Card class="w-full md:w-6 borda">
+          <template #title >
+            <div id="titulo">Cadastro de Turmas </div>
+          </template>
+
+           <template #content>
+            <div class="p-fluid">
+
+                <div class="field">
+                    <label for="email">Email</label>
+                    <InputText id="email" v-model="form.email" />
+                </div>
+
+                <div class="field mt-3">
+                    <label for="senha">Senha</label>
+                    <Password id="senha" v-model="form.senha" toggle-mask />
+                </div>
+                <Button 
+                    label="Entrar"
+                    icon="pi pi-user-plus"
+                    class="mt-3"
+                    @click="login"
+                    :loading="loading"
+                />    
+            </div>
+            <div class="footer"> &copy; 2025 Sabiá</div>
+        </template>
+        </Card>
+    </div>
+</template>
